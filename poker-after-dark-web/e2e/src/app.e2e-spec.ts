902ede5e4947804f61cf0b0d33cdb0971e8e79bb
Dominik93 <slusarz.dominik@gmail.com>
2020-01-10 21:21:41 +0100

    review- remove legacy names

--- poker-after-dark-web/e2e/src/app.e2e-spec.ts
+++ poker-after-dark-web/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to pokerstats-web!');
+    expect(page.getParagraphText()).toEqual('Welcome to poker-after-dark-web!');
   });
 });
--- poker-after-dark-web/src/app/app.component.spec.ts
+++ poker-after-dark-web/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'pokerstats-web'`, async(() => {
+  it(`should have as title 'poker-after-dark-web'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('pokerstats-web');
+    expect(app.title).toEqual('poker-after-dark-web');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pokerstats-web!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to poker-after-dark-web!');
   }));
 });
