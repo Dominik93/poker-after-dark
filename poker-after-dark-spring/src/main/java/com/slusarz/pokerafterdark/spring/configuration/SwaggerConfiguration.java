7a9a6a6e6e13e72382d7f7f2f20d0d8cbf90f740
Dominik93 <slusarz.dominik@gmail.com>
2020-01-25 09:09:05 +0100

    PAD-12

--- poker-after-dark-spring/src/main/java/com/slusarz/pokerafterdark/spring/configuration/SwaggerConfiguration.java
+++ poker-after-dark-spring/src/main/java/com/slusarz/pokerafterdark/spring/configuration/SwaggerConfiguration.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 import static springfox.documentation.builders.PathSelectors.any;
 
@@ -24,12 +29,25 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(any())
                 .build()
-                .apiInfo(testApiInfo());
+                .globalOperationParameters(Arrays.asList(authenticationParameter()))
+                .apiInfo(apiInfo());
     }
 
-    private ApiInfo testApiInfo() {
-        ApiInfo apiInfo = new ApiInfoBuilder().title("Test APIs").description("GET POST PUT methods are supported ").version("V1").build();
-        return apiInfo;
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Poker After Dark API")
+                .description("Swagger for Poker After Dark")
+                .version("V1").build();
+    }
+
+    public Parameter authenticationParameter() {
+        return new ParameterBuilder()
+                .name("Authentication")
+                .defaultValue("")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
     }
 
 }
